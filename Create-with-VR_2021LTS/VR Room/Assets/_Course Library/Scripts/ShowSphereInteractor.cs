491622961bfbc02efee1cc38b4bc555df59be52e
Gabriel Blackwell <gabrielsblackwell@gmail.com>
2023-06-21 02:34:15 -0400

    Added new kinova model, not rigged, made interaction bubbles around each joint of the simplistic robot and a script that will eventually show on hover from rayhit

---
+++ Create-with-VR_2021LTS/VR Room/Assets/_Course Library/Scripts/ShowSphereInteractor.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowSphereInteractor : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject.Find("RightHand Controller").GetComponent<XR Interactor Line Visual>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var ray = 
+        if (ray, )
+    }
+}
