8d280e0328bbd7945316f5e6fd94da96bdb01eec
Caleb <calebhenry2003@gmail.com>
2024-02-12 15:38:08 -0500

    Replaced arm model

--- Create-with-VR_2021LTS/HCITECH-Robotics_Project/Assets/_Course Library/Scripts/Custom Scripts/RobotManager.cs
+++ Create-with-VR_2021LTS/HCITECH-Robotics_Project/Assets/_Course Library/Scripts/Custom Scripts/RobotManager.cs
@@ -9,19 +9,19 @@ public class RobotManager : MonoBehaviour
     void Start()
     {
         // Add positions to dictionary
-        reset_positions.Add("Base - Pivot", new Vector3(0f, 0.0009999871f, 0.174f));
-        reset_positions.Add("Segment 1 - Pivot", new Vector3(-0.00999999f, 0.0009999871f, 0.274f));
-        reset_positions.Add("Segment 2 - Pivot", new Vector3(0.01999998f, 0.1729f, 0.6472001f));
-        reset_positions.Add("Segment 3 - Pivot", new Vector3(0.5471001f, 1.5861f, 0.01090002f));
-        reset_positions.Add("Segment 4 - Pivot", new Vector3(-0.009999991f, -0.01300001f, 0.4532f));
-        reset_positions.Add("Segment 5 - Pivot", new Vector3(0.5983006f, 1.447901f, 0.008399725f));
+        reset_positions.Add("Base - Pivot", new Vector3(0,0.000999987125f,0.174000025f)); 
+        reset_positions.Add("Segment 1 - Pivot", new Vector3(-0.00999999046f,0.000999987125f,0.274000049f));
+        reset_positions.Add("Segment 2 - Pivot", new Vector3(0.0185010433f,0.149899304f,0.524781704f));
+        reset_positions.Add("Segment 3 - Pivot", new Vector3(0.516099989f,1.71879995f,0.0181999207f));
+        reset_positions.Add("Segment 4 - Pivot", new Vector3(-0.00999999139f,-0.0130000114f,0.453200042f));
+        reset_positions.Add("Segment 5 - Pivot", new Vector3(0.598300636f,1.44790101f,0.00839972496f));
         // Add rotations to dictionary
         reset_rotations.Add("Base - Pivot", new Quaternion(0, 0, 0, 1));
         reset_rotations.Add("Segment 1 - Pivot", new Quaternion(0, 0, 0, 1));
         reset_rotations.Add("Segment 2 - Pivot", new Quaternion(0, 0, 0, 1));
-        reset_rotations.Add("Segment 3 - Pivot", new Quaternion(-0.669509113f, 0.227502942f, 0.669509113f, 0.227502942f));
-        reset_rotations.Add("Segment 4 - Pivot", new Quaternion(0.0990288109f, 0, 0, 0.995084584f));
-        reset_rotations.Add("Segment 5 - Pivot", new Quaternion(-0.664092183f, 0.242861241f, 0.664092183f, 0.242861241f));
+        reset_rotations.Add("Segment 3 - Pivot", new Quaternion(0.706698656f,0.024020819f,-0.706698656f,0.0240208823f));
+        reset_rotations.Add("Segment 4 - Pivot", new Quaternion(0.0990288109f,0,0,0.995084584f));
+        reset_rotations.Add("Segment 5 - Pivot", new Quaternion(-0.664092183f,0.242861241f,0.664092183f,0.242861241f));
     }
     private float speedModifier = 1f;
     public void ResetSpeedModifier()
